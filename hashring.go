94549d644df36787378947137090a9902f7cda77
vedhavyas <vedhavyas.singareddi@gmail.com>
2017-12-13 17:55:31 +0530

    add nodes to hashring

---
+++ hashring.go
@@ -0,0 +1,67 @@
+package hashring
+
+import (
+	"fmt"
+	"hash"
+	"hash/fnv"
+	"sort"
+	"sync"
+)
+
+// nodeIdx type implementing Sort Interface
+type nodeIdx []uint32
+
+// Len returns the size of nodeIdx
+func (idx nodeIdx) Len() int {
+	return len(idx)
+}
+
+// Swap swaps the ith with jth
+func (idx nodeIdx) Swap(i, j int) {
+	idx[i], idx[j] = idx[j], idx[i]
+}
+
+// Less returns true if ith <= jth else false
+func (idx nodeIdx) Less(i, j int) bool {
+	return idx[i] <= idx[j]
+}
+
+// HashRing to hold the nodes and indexes
+type HashRing struct {
+	nodes            map[uint32]string // map to idx -> node
+	idx              nodeIdx           // sorted indexes
+	virtualNodeCount int               // virtual nodes to be inserted
+	hash             hash.Hash32
+	mu               sync.RWMutex // to protect above fields
+}
+
+// New returns a Hash ring with provided virtual node count
+func New(virtualNodeCount int) *HashRing {
+	return &HashRing{
+		nodes:            make(map[uint32]string),
+		virtualNodeCount: virtualNodeCount,
+		hash:             fnv.New32a(),
+	}
+}
+
+// AddNode adds a node to Hash ring
+func (hr *HashRing) AddNode(node string) error {
+	hr.mu.Lock()
+	defer hr.mu.Unlock()
+
+	for i := 0; i < hr.virtualNodeCount; i++ {
+		key := fmt.Sprintf("%s:%d", node, i)
+		hr.hash.Reset()
+		_, err := hr.hash.Write([]byte(key))
+		if err != nil {
+			return fmt.Errorf("failed to add node: %v", err)
+		}
+
+		hkey := hr.hash.Sum32()
+		hr.idx = append(hr.idx, hkey)
+		hr.nodes[hkey] = node
+	}
+
+	sort.Sort(hr.idx)
+	return nil
+}
---
+++ hashring_test.go
@@ -0,0 +1,90 @@
+package hashring
+
+import (
+	"reflect"
+	"sort"
+	"testing"
+)
+
+func TestNodeIdx_Sort(t *testing.T) {
+	tests := []struct {
+		v nodeIdx
+		r nodeIdx
+	}{
+		{
+			v: nodeIdx{5, 1, 3, 9, 10, 0, 78},
+			r: nodeIdx{0, 1, 3, 5, 9, 10, 78},
+		},
+
+		{
+			v: nodeIdx{9, 3, 5, 4, 2, 3, 6},
+			r: nodeIdx{2, 3, 3, 4, 5, 6, 9},
+		},
+	}
+
+	for _, c := range tests {
+		sort.Sort(c.v)
+		if !reflect.DeepEqual(c.v, c.r) {
+			t.Fatalf("expected %v but got %v\n", c.r, c.v)
+		}
+	}
+}
+
+func TestHashRing_AddNode(t *testing.T) {
+	tests := []struct {
+		virtualNodeCount int
+		nodes            []string
+		requiredCount    int
+	}{
+		{
+			virtualNodeCount: 1,
+			nodes:            []string{"abc_node"},
+			requiredCount:    1,
+		},
+
+		{
+			virtualNodeCount: 3,
+			nodes:            []string{"abc_node"},
+			requiredCount:    3,
+		},
+
+		{
+			virtualNodeCount: 4,
+			nodes:            []string{"abc_1", "abc_2"},
+			requiredCount:    8,
+		},
+
+		{
+			virtualNodeCount: 1,
+			nodes:            []string{"abc_1", "abc_2", "abc_3"},
+			requiredCount:    3,
+		},
+	}
+
+	testContains := func(hr *HashRing, nodes []string) bool {
+		result := true
+		for _, node := range nodes {
+			count := 0
+			for _, v := range hr.nodes {
+				if v == node {
+					count++
+				}
+			}
+
+			result = result && (count == hr.virtualNodeCount)
+		}
+
+		return result
+	}
+
+	for _, c := range tests {
+		hr := New(c.virtualNodeCount)
+		for _, node := range c.nodes {
+			hr.AddNode(node)
+		}
+
+		if !(testContains(hr, c.nodes) && len(hr.idx) == c.virtualNodeCount*len(c.nodes)) {
+			t.Fatalf("node count didn't match: %+v\n", hr)
+		}
+	}
+}
